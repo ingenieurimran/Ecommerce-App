4013f1604cadc5389e38088648eed93c64c9864b
Imran Ali <engr.imran_1242@hotmail.com>
2022-10-20 00:37:27 +0200

    Search-with-keyword, Normal-filter, Range-filter and Pagination Part

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require('../model/productModel')
 const ErrorHandler = require('../utils/errorhandler')
 
 const catchAsyncErrors = require('../middleware/catchAsyncErrors')
+const ApiFeatures = require('../utils/apifeatures')
 
 // Create Product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,7 +14,11 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 })
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find()
+  const resultPerPage = 5
+  const productCount = await Product.countDocuments()
+  const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage)
+
+  const products = await apiFeature.query
   res.status(200).json({
     success: true,
     products,
@@ -31,7 +36,8 @@ exports.getProductDetails = catchAsyncErrors(async(req, res, next)=>{
 
   res.status(200).json({
     success: true,
-    product
+    product,
+    productCount
   })
 
 })
